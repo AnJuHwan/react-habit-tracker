6e63e91068a6d49e29bd9d0b8c253cf595bd335b
wnghks0531 <wnghks0531>
2020-09-29 08:51:13 +0900

    Updates

---
+++ precache-manifest.3c61ea375534ff0adc891f464c55d332.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d1392cb9bad601c1d828c7186869a2",
+    "url": "/react-habit-tracker/index.html"
+  },
+  {
+    "revision": "3afa04834b19d0106657",
+    "url": "/react-habit-tracker/static/css/main.37dc3757.chunk.css"
+  },
+  {
+    "revision": "feae5cd7dbbcef79255c",
+    "url": "/react-habit-tracker/static/js/2.c6970153.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/react-habit-tracker/static/js/2.c6970153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3afa04834b19d0106657",
+    "url": "/react-habit-tracker/static/js/main.1936301c.chunk.js"
+  },
+  {
+    "revision": "042765e3f71c043ed96c",
+    "url": "/react-habit-tracker/static/js/runtime-main.f843a2cf.js"
+  }
+]);
